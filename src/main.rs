790539b3e71a362c5ce5aedba8084e0a0fd57b33
Maksym Tkachov <maksym.tkachov@sigma.software>
2024-03-12 09:00:51 +0100

    2

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,37 @@ fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
     k as i32
 }
 
+fn str_str(haystack: String, needle: String) -> i32 {
+    let mut str = haystack.chars().skip(0);
+    let mut s = needle.chars();
+    let mut i = 0;
+    let mut j = 0;
+
+    loop {
+        let c = str.next();
+        let v = s.next();
+
+        if c == None || v == None { 
+            break;
+        }
+
+        if c.unwrap() == v.unwrap() {
+            i += 1;
+            j += 1;
+        } else {
+            i -= j;
+            i += 1;
+            str = haystack.chars().skip(i);
+            s = needle.chars();
+            j = 0;
+        }
+    }
+    if j >= needle.len() {
+        return (i - j) as i32;
+    }
+    -1
+}
+
 fn main() {
     let mut v = vec![1];
     let r = remove_duplicates(&mut v);
